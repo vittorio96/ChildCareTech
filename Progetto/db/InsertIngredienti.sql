2517beb720dbd0d7e9292de9ac716e29a90f3292
rfma23 <rafael.mosca@mail.polimi.it>
2018-04-19 00:51:24 +0200

    Enjoy Child Care Evolution The new version of Child Care Tech

---
+++ Progetto/db/InsertIngredienti.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM ChildCareTech.Ingrediente
+LIMIT 0, 1000
+
+-- Date: 2018-04-18 10:17
+*/
+INSERT INTO `Ingrediente` (`NomeI`) VALUES ('BASILICO');
+INSERT INTO `Ingrediente` (`NomeI`) VALUES ('MOZZARELLA');
+INSERT INTO `Ingrediente` (`NomeI`) VALUES ('PARMIGIANO');
+INSERT INTO `Ingrediente` (`NomeI`) VALUES ('PENNETE');
+INSERT INTO `Ingrediente` (`NomeI`) VALUES ('RICOTTA');
+INSERT INTO `Ingrediente` (`NomeI`) VALUES ('SUGO DI POMODORO');
